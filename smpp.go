a8bde90f1518a03a55aa1e7f2079c433a20f9efa
Dima Romaniuk <d.romaniuk@gms-worldwide.com>
2025-11-04 12:55:08 +0200

    Fix metrics

--- smpp.go
+++ smpp.go
@@ -2,7 +2,6 @@ package smpp
 
 import (
     "fmt"
-    "log"
     "time"
 
     "github.com/fiorix/go-smpp/smpp"
@@ -14,45 +13,48 @@ import (
 
 type SMPP struct{}
 
+type Session struct {
+    tx        *smpp.Transmitter
+    latency   *metrics.Metric
+    successes *metrics.Metric
+    failures  *metrics.Metric
+}
+
 type Config struct {
     Host       string
     Port       int
     SystemID   string
     Password   string
     SystemType string
-    Bind       string // "transmitter" | "transceiver"
 }
 
-type Session struct {
-    tx          *smpp.Transceiver
-    submitTrend *metrics.Trend
-    dlrTrend    *metrics.Trend
-    successes   *metrics.Counter
-    failures    *metrics.Counter
-}
-
-// K6 instance
-func (s *SMPP) XNewModuleInstance(vu modules.VU) modules.Instance {
-    rt := vu.InitEnv().Registry
-    return &SMPPInstance{
-        vu: vu,
-        submitTrend: rt.NewTrend("smpp_submit_latency", metrics.TrendTime, metrics.Default),
-        dlrTrend:    rt.NewTrend("smpp_dlr_latency", metrics.TrendTime, metrics.Default),
-        successes:   rt.NewCounter("smpp_submit_success"),
-        failures:    rt.NewCounter("smpp_submit_failure"),
-    }
+func init() {
+    modules.Register("k6/x/smpp", new(SMPP))
 }
 
 type SMPPInstance struct {
-    vu          modules.VU
-    submitTrend *metrics.Trend
-    dlrTrend    *metrics.Trend
-    successes   *metrics.Counter
-    failures    *metrics.Counter
+    vu        modules.VU
+    latency   *metrics.Metric
+    successes *metrics.Metric
+    failures  *metrics.Metric
 }
 
 var _ modules.Module = &SMPP{}
 var _ modules.Instance = &SMPPInstance{}
+
+func (m *SMPP) NewModuleInstance(vu modules.VU) modules.Instance {
+    reg := vu.InitEnv().Registry
+    latency := reg.MustNewMetric("smpp_submit_latency", metrics.Trend)
+    successes := reg.MustNewMetric("smpp_submit_success", metrics.Counter)
+    failures := reg.MustNewMetric("smpp_submit_failure", metrics.Counter)
+
+    return &SMPPInstance{
+        vu:        vu,
+        latency:   latency,
+        successes: successes,
+        failures:  failures,
+    }
+}
 
 func (i *SMPPInstance) Exports() modules.Exports {
     return modules.Exports{
@@ -65,43 +67,28 @@ func (i *SMPPInstance) Exports() modules.Exports {
 
 func (i *SMPPInstance) Connect(cfg Config) (*Session, error) {
     addr := fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
-
-    // Callback для отримання deliver_sm
-    rx := func(pdu smpp.PDU) {
-        if pdu == nil {
-            return
-        }
-        if pdu.Header().ID.String() == "deliver_sm" {
-            i.dlrTrend.Add(0.001) // фіксуємо факт отримання, latency можна обчислити у майбутньому
-            log.Printf("[SMPP] Received deliver_sm: %+v\n", pdu)
-        }
-    }
-
-    tr := &smpp.Transceiver{
+    tx := &smpp.Transmitter{
         Addr:       addr,
         User:       cfg.SystemID,
         Passwd:     cfg.Password,
         SystemType: cfg.SystemType,
-        Handler:    smpp.HandlerFunc(rx),
     }
 
-    conn := tr.Bind()
+    conn := tx.Bind()
     select {
     case status := <-conn:
         if status.Status().Error() != nil {
             return nil, status.Status().Error()
         }
-        log.Printf("[SMPP] Connected to %s", addr)
     case <-time.After(5 * time.Second):
         return nil, fmt.Errorf("bind timeout")
     }
 
     return &Session{
-        tx:          tr,
-        submitTrend: i.submitTrend,
-        dlrTrend:    i.dlrTrend,
-        successes:   i.successes,
-        failures:    i.failures,
+        tx:        tx,
+        latency:   i.latency,
+        successes: i.successes,
+        failures:  i.failures,
     }, nil
 }
 
@@ -115,19 +102,19 @@ func (s *Session) SendSMS(src, dst, text string) (string, error) {
         Src:      src,
         Dst:      dst,
         Text:     pdutext.Latin1(text),
-        Register: smpp.FinalDeliveryReceipt, // просимо SMSC надіслати DLR
+        Register: smpp.NoDeliveryReceipt,
     }
 
     resp, err := s.tx.Submit(msg)
     duration := time.Since(start)
-    s.submitTrend.Add(duration.Seconds())
 
+    metrics.PushIfNotDone(s.latency, duration.Seconds())
     if err != nil {
-        s.failures.Add(1)
+        metrics.PushIfNotDone(s.failures, 1)
         return "", err
     }
 
-    s.successes.Add(1)
+    metrics.PushIfNotDone(s.successes, 1)
     msgID, _ := resp.Fields[pdufield.MessageID].String()
     return msgID, nil
 }
